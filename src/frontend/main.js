ced688aad8e5390edb74426120b319a054a1255f
Leo Emeri <leo.emeri@gmail.com>
2025-01-15 15:46:06 +0100

    Fix css files mime type

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -4,7 +4,7 @@ import router from './router';
 import { setDefaultBaseUrl, setDefaultHeaders } from './composables/useFetchApi';
 
 // Définir l'URL de base
-setDefaultBaseUrl('https://archioweb-incrementalgame.onrender.com');
+setDefaultBaseUrl('https://devmobil-incrementalgame.onrender.com');
 
 // Ajouter le token d'authentification aux headers par défaut
 const token = localStorage.getItem('token');
